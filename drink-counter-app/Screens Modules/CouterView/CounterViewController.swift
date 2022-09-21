f200492f8527d1858cdf5d8c013761c4ed3c7b87
Felipe R <felipe1319@gmail.com>
2022-09-21 14:31:24 -0500

    first commit

--- drink-counter-app/Screens Modules/CouterView/CounterViewController.swift
+++ drink-counter-app/Screens Modules/CouterView/CounterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CounterViewController.swift
 //  drink-counter-app
 //
 //  Created by Felipe on 21/09/22.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CounterViewController: UIViewController {
+    fileprivate var viewModel = CounterViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
---
+++ drink-counter-app/Screens Modules/CouterView/CounterViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CounterViewModel.swift
+//  drink-counter-app
+//
+//  Created by Felipe on 21/09/22.
+//
+
+import Foundation
+
+
+struct CounterViewModel {
+    
+}
