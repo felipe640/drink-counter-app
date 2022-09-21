f12452e16314b9ee1e197aa003c05778f5757c7d
Felipe R <felipe1319@gmail.com>
2022-09-21 14:00:19 -0500

    Initial Commit

---
+++ drink-counter-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  drink-counter-app
+//
+//  Created by Felipe on 21/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
